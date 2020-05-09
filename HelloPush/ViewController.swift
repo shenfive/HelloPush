7334ed2a473c73cc2116a7a03f01843e6a0fd8bb
Danny Shen <shenfive@gmail.com>
2020-05-09 09:53:12 +0800

    Initial Commit

---
+++ HelloPush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloPush
+//
+//  Created by 申潤五 on 2020/5/9.
+//  Copyright © 2020 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
